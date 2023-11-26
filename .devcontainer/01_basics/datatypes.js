82fb1b772b6f1cf143398458c72a18e83532e8e5
Amar pratap <144508649+phenomAP@users.noreply.github.com>
2023-11-26 06:07:18 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using node js not browser
+
+let name="Amar"
+let age=20
+let loggedIn=true
+
+//number=> 2 to power 53
+//bigint
+//string=>" "
+//boolean=>true/false
+//null=>standalone value
+//undefined
+//symbol=unique
+//object
+console.log(typeof"Amar");
+console.log(typeof age);
+console.log(typeof null)//object
+console.log(typeof undefined);
